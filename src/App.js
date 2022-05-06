426144782d2245431a830331ec3f00ad8d2aced7
imharsh <imharsh098@gmail.com>
2022-05-06 11:25:19 +0530

    first commit

---
+++ src/App.js
@@ -0,0 +1,8 @@
+import "./App.css";
+import SideNav from "./Components/SideNav.js";
+
+function App() {
+  return <SideNav />;
+}
+
+export default App;
---
+++ src/Components/SideNav.js
@@ -0,0 +1,99 @@
+import React from "react";
+
+function SideNav() {
+  return (
+    <div className="container">
+      <div className="sidebar">
+        <ul className="nav-list">
+          <li>
+            <a href="#">
+              <i className="bx bxs-dashboard"></i>
+              <span className="links-name">Dashboard</span>
+            </a>
+          </li>
+          <li>
+            <a href="#">
+              <i className="bx bx-user"></i>
+              <span className="links-name">Users</span>
+            </a>
+          </li>
+          <li>
+            <a href="#">
+              <i className="bx bx-list-ul"></i>
+              <span className="links-name">Categories</span>
+            </a>
+          </li>
+          <li>
+            <a href="#">
+              <i className="bx bx-flag"></i>
+              <span className="links-name">Banner</span>
+            </a>
+          </li>
+          <li>
+            <a href="#">
+              <i className="bx bxs-truck"></i>
+              <span className="links-name">Delivery Profile</span>
+            </a>
+          </li>
+          <li>
+            <a href="#">
+              <i className="bx bx-group"></i>
+              <span className="links-name">Vendors</span>
+            </a>
+          </li>
+          <li>
+            <a href="#">
+              <i className="bx bxs-dashboard"></i>
+              <span className="links-name">Products</span>
+            </a>
+          </li>
+
+          <li>
+            <a href="#">
+              <i className="bx bx-cart"></i>
+              <span className="links-name">Order</span>
+            </a>
+          </li>
+
+          <li>
+            <a href="#">
+              <i className="bx bxs-dashboard"></i>
+              <span className="links-name">Transaction</span>
+            </a>
+          </li>
+
+          <li>
+            <a href="#">
+              <i className="bx bxs-dashboard"></i>
+              <span className="links-name">Payment Method</span>
+            </a>
+          </li>
+
+          <li>
+            <a href="#">
+              <i className="bx bxs-dashboard"></i>
+              <span className="links-name">Faqs</span>
+            </a>
+          </li>
+
+          <li>
+            <a href="#">
+              <i className="bx bx-phone-call"></i>
+              <span className="links-name">Support</span>
+            </a>
+          </li>
+
+          <li>
+            <a href="#">
+              <i className="bx bxs-dashboard"></i>
+              <span className="links-name">Settings</span>
+            </a>
+          </li>
+        </ul>
+      </div>
+      <div className="next">vjhb</div>
+    </div>
+  );
+}
+
+export default SideNav;
